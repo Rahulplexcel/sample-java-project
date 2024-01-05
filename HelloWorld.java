038030834996788e98da14e72bd8cecb91f7f880
Rahulplexcel <123854462+Rahulplexcel@users.noreply.github.com>
2024-01-05 16:11:59 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,5 @@
-class Program1
-  {
-    public static void main[args[]str]
-    {
-      System.out.println("Im Rahul");
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
     }
-  }
+}
